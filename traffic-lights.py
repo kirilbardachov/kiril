69184aa9486c3363014dd482230818f605dcc907
kirilbardachov <48856115+kirilbardachov@users.noreply.github.com>
2019-03-27 14:29:19 +0200

    Update traffic-lights.py
    bitch

--- traffic-lights.py
+++ traffic-lights.py
@@ -1,13 +1,1 @@
-from ev3dev2.led import Leds
-import time
-lights=Leds()
-while True:
-  lights.set_color("RIGHT","RED")
-  lights.set_color("LEFT","RED")
-  time.sleep(1)
-  lights.set_color("RIGHT","AMBER")
-  lights.set_color("LEFT","AMBER")
-  time.sleep(1)
-  lights.set_color("RIGHT","GREEN")
-  lights.set_color("LEFT","GREEN")
-  time.sleep(1)
+u have to exit from accounts in public places
